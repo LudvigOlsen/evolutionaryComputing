cbab6c20506e8b52d951c256abf136a5de09fc98
Ludvig Renbo Olsen <mail@ludvigolsen.dk>
2018-09-11 16:23:19 +0200

    minor

--- assignmentfiles_2018/assignmentfiles_2017/charles/breeders/SimpleBreeder.java
+++ assignmentfiles_2018/assignmentfiles_2017/charles/breeders/SimpleBreeder.java
@@ -1,4 +1,49 @@
 package charles.breeders;
 
-public class SimpleBreeder {
+import charles.Individual;
+import charles.Population;
+import charles.mutators.Mutator;
+import charles.parentSelectors.ParentSelector;
+import charles.recombinators.Recombinator;
+
+public class SimpleBreeder implements Breeder {
+
+    private ParentSelector parentSelector;
+    private Recombinator recombinator;
+    private Mutator mutator;
+    private double minLimit;
+    private double maxLimit;
+
+
+    public SimpleBreeder(ParentSelector parentSelector, Recombinator recombinator, Mutator mutator,
+                         double minLimit, double maxLimit) {
+        this.parentSelector = parentSelector;
+        this.recombinator = recombinator;
+        this.mutator = mutator;
+        this.minLimit = minLimit;
+        this.maxLimit = maxLimit;
+    }
+
+    @Override
+    public Population breedChildren(Population population, int numParents, int numChildren) {
+
+        Population children = new Population();
+
+        for (int ch = 0; ch < numChildren; ch++) {
+            // Select parents
+            Population parents = parentSelector.selectParents(population, numParents);
+
+            // Create Children
+            // Recombination
+            Individual child = recombinator.combine(parents, minLimit, maxLimit);
+            // Mutation
+            mutator.mutate(child);
+
+            // Add child to population
+            children.addIndividual(child);
+        }
+
+        return children;
+
+    }
 }
