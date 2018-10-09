cb92009741cbd3e7e9c3fd0b7ccde4c27f560010
Pieter <oyenspieter@gmail.com>
2018-10-09 18:44:13 +0200

    Late push

--- assignmentfiles_2018/assignmentfiles_2017/runntests.sh
+++ assignmentfiles_2018/assignmentfiles_2017/runntests.sh
@@ -3,29 +3,21 @@ evaluationType=$1
 numberOfTests=$2
 fileName=${evaluationType}Results 
 touch Results/$fileName.txt
-echo "Run # | Score" > Results/$fileName.txt
-echo "Run # | Score" 
-echo "---------------------------------"
+echo "Run # | Seed | Score" > Results/$fileName.txt
+printf "\n%-5s %4s %7s\n" "Run #" "Seed" "Score"
+printf "=======================================\n"
 echo "---------------------------------" >> Results/$fileName.txt
 for ((i=1; i<=$numberOfTests; i++))
 do
-    result=$(java -jar testrun.jar -submission=player56 -evaluation=$evaluationType -seed=$i)
+    randomSeed=$(( RANDOM % 10000 ))
+    result=$(java -jar testrun.jar -submission=player56 -evaluation=$evaluationType -seed=$randomSeed)
     resultArray=($result)
     #echo $result
-    
-    if [ $i -lt 10 ]
-    then 
-        echo "$i     | ${resultArray[1]}" >> Results/$fileName.txt
-        echo "$i     | ${resultArray[1]}"
-    elif [ $i -lt 100 ]
-    then 
-        echo "$i    | ${resultArray[1]}" >> Results/$fileName.txt
-        echo "$i    | ${resultArray[1]}"
-    else
-        echo "$i   | ${resultArray[1]}" >> Results/$fileName.txt
-        echo "$i   | ${resultArray[1]}"
-    fi
+    printf "%-5d %4d %7.20f" "$i" "$randomSeed" "${resultArray[1]}" #>> Results/$fileName.txt
+    printf "\n" #>> Results/$fileName.txt
+    #printf "%-10s%-4s|\n"
 
-    
-    #echo "$result" >> BentResults.txt
+    #echo "$i     | $randomSeed    |${resultArray[1]}" >> Results/$fileName.txt
+
+
 done
